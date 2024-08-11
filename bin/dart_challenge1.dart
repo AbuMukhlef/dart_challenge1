621ea04d36b11fbc7433e70839e6a032abc2372b
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-11 21:18:27 +0300

    solved

--- bin/dart_challenge1.dart
+++ bin/dart_challenge1.dart
@@ -1,5 +1,30 @@
-import 'package:dart_challenge1/dart_challenge1.dart' as dart_challenge1;
+/*
+Create a Dart program that uses a `Map` to manage recipes. Each recipe is stored with a name (String) as the key
+ and a list of ingredients (List of Strings) as the value. Implement functions to:
 
+- Add a new recipe.
+- Remove a recipe by name.
+- List all recipes and their ingredients.
+- Search for a recipe by name and display its ingredients.
+*/
 void main(List<String> arguments) {
-  print('Hello world: ${dart_challenge1.calculate()}!');
+  Map<String, List<String>> recipes = {
+    'Pancakes': ['eggs', 'flour', 'sugar', 'milk', 'butter'],
+    'Spaghetti': ['tomatoes', 'onions', 'pepper', 'spaghetti'],
+  };
+  // Add a new recipe
+  recipes['Lasagna'] = ['eggs', 'flour', 'sugar', 'milk', 'butter'];
+  // List all recipes and their ingredients
+  print('____________******_____________');
+  print(recipes);
+  // Remove a recipe
+  recipes.remove('Pancakes');
+  // List all recipes and their ingredients
+  print('____________######_____________');
+  print(recipes);
+  // Search for a recipe by name and display its ingredients
+  print('____________***_____________');
+  if (recipes.containsKey('Spaghetti')) {
+    print(recipes['Spaghetti']);
+  }
 }
